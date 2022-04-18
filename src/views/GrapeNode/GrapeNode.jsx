39e1307f6c534e7cb56e4feb5b280e9308a4a2e3
quartz-defi <quartz.defi@protonmail.com>
2022-04-18 15:02:07 +0100

    Fix tupo

--- src/views/GrapeNode/GrapeNode.jsx
+++ src/views/GrapeNode/GrapeNode.jsx
@@ -5,7 +5,7 @@ import { Route, Switch, useParams, useRouteMatch } from 'react-router-dom';
 import { useWallet } from 'use-wallet';
 import PageHeader from '../../components/PageHeader';
 import { Box, Card, CardContent, Typography, Grid, MenuItem, withStyles } from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import styled from 'styled-components';
 import Spacer from '../../components/Spacer';
 import Harvest from './components/Harvest';
@@ -19,7 +19,7 @@ import totalNodes from '../../hooks/useTotalNodes';
 import useStatsForPool from '../../hooks/useStatsForPool';
 import useStakedTokenPriceInDollars from '../../hooks/useStakedTokenPriceInDollars';
 
-import {Alert} from '@material-ui/lab';
+import { Alert } from '@material-ui/lab';
 
 const useStyles = makeStyles((theme) => ({
   gridItem: {
@@ -52,135 +52,131 @@ const GrapeNode = () => {
   );
 
   return (
-    
-      <>
-        <Helmet>
-          <title>{TITLE}</title>
-        </Helmet>
-        {!!account ? (
-          <>
-            {
-              bank
-  ? (
-      <>
-        <Route exact path={path}>
-              <Box mb={4}>
-                <Typography
-                  style={{ textTransform: 'none', fontWeight: 'bold' }}
-                  color="textPrimary"
-                  align="center"
-                  variant="h3"
-                  gutterBottom
-                >
-                  Buy &amp; Redeem Bonds
-                </Typography>
-                <Typography
-                  style={{ textTransform: 'none' }}
-                  color="textPrimary"
-                  align="center"
-                  variant="h5"
-                  gutterBottom
-                >
-                  Earn premiums upon redemption
-                </Typography>
-              </Box>
-            </Route>
-        {/* <Button onClick={setTierValues}>Set Tier Values</Button> */}
-        <Box>
-          <Grid container justify="center" spacing={2} style={{marginBottom: '50px', marginTop: '20px'}}>
-          
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-            
-                <Card className={classes.gridItem}>
-                  <CardContent style={{ textAlign: 'center' }}>
-                    <Typography>Your Nodes | TVL</Typography>
-                    <Typography>
-                      {
-                        nodes[0] &&
-                        <>
-                          <span style={{ color: 'rgb(0, 0, 0)', marginRight: '0px' }}>
-                            {nodes[0].toString()}
-                          </span> |  <span style={{ color: 'rgb(0, 0, 0)', marginRight: '0px' }}>
-                            ${bank.depositTokenName === 'AMES' ? (nodes[0] * (tokenPriceInDollars*50)).toFixed(0) : (nodes[0] * (tokenPriceInDollars*0.5)).toFixed(0)}
-                           
-                          </span>
-                          
-                        </>
-                      }
-                    </Typography>
-                  </CardContent>
-                </Card>
-         
-            </Grid>
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-              <Card className={classes.gridItem}>
-                <CardContent style={{textAlign: 'center'}}>
-                  <Typography>Amount Claimed</Typography>
-                  <Typography>{(Number(userDetails.total_claims)/1e18).toFixed(2)} {bank.earnTokenName}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-              <Card className={classes.gridItem}>
-                <CardContent style={{textAlign: 'center'}}>
-                  <Typography>Max Possible Pay</Typography>
-                  <Typography>{Number(max)/1e18} {bank.earnTokenName}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-              <Card className={classes.gridItem}>
-                <CardContent style={{textAlign: 'center'}}>
-                  <Typography>APR | Daily</Typography>
-                  <Typography>{bank.closedForStaking ? '0.00' : statsOnPool?.yearlyAPR}% | {bank.closedForStaking ? '0.00' : statsOnPool?.dailyAPR}%</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-              <Card className={classes.gridItem}>
-                <CardContent style={{textAlign: 'center'}}>
-                  <Typography>Total {bank.earnTokenName} Nodes</Typography>
-                  <Typography>{Number(total[0])}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          
-            <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
-              <Card className={classes.gridItem}>
-                <CardContent style={{textAlign: 'center'}}>
-                  <Typography>TVL</Typography>
-                  <Typography>${statsOnPool?.TVL ? (Number((Number(statsOnPool?.TVL).toFixed(0)))).toLocaleString('en-US') : '-.--'}</Typography>
-                </CardContent>
-              </Card>
-            </Grid>
-          </Grid>
-        </Box>
+    <>
+      <Helmet>
+        <title>{TITLE}</title>
+      </Helmet>
+      {!!account ? (
+        <>
+          {bank ? (
+            <>
+              <Route exact path={path}>
+                <Box mb={4}>
+                  <Typography
+                    style={{ textTransform: 'none', fontWeight: 'bold' }}
+                    color="textPrimary"
+                    align="center"
+                    variant="h3"
+                    gutterBottom
+                  >
+                    Generate Ames with Nodes
+                  </Typography>
+                </Box>
+              </Route>
+              {/* <Button onClick={setTierValues}>Set Tier Values</Button> */}
+              <Box>
+                <Grid container justify="center" spacing={2} style={{ marginBottom: '50px', marginTop: '20px' }}>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>Your Nodes | TVL</Typography>
+                        <Typography>
+                          {nodes[0] && (
+                            <>
+                              <span style={{ color: 'rgb(0, 0, 0)', marginRight: '0px' }}>{nodes[0].toString()}</span> |{' '}
+                              <span style={{ color: 'rgb(0, 0, 0)', marginRight: '0px' }}>
+                                $
+                                {bank.depositTokenName === 'AMES'
+                                  ? (nodes[0] * (tokenPriceInDollars * 50)).toFixed(0)
+                                  : (nodes[0] * (tokenPriceInDollars * 0.5)).toFixed(0)}
+                              </span>
+                            </>
+                          )}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>Amount Claimed</Typography>
+                        <Typography>
+                          {(Number(userDetails.total_claims) / 1e18).toFixed(2)} {bank.earnTokenName}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>Max Possible Pay</Typography>
+                        <Typography>
+                          {Number(max) / 1e18} {bank.earnTokenName}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>APR | Daily</Typography>
+                        <Typography>
+                          {bank.closedForStaking ? '0.00' : statsOnPool?.yearlyAPR}% |{' '}
+                          {bank.closedForStaking ? '0.00' : statsOnPool?.dailyAPR}%
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>Total {bank.earnTokenName} Nodes</Typography>
+                        <Typography>{Number(total[0])}</Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
 
-        <Box mt={5}>
-          <StyledBank>
-            <StyledCardsWrapper>
-              <StyledCardWrapper>
-                <Harvest bank={bank} />
-              </StyledCardWrapper>
-              <Spacer />
-              <StyledCardWrapper>{<Stake bank={bank} />}</StyledCardWrapper>
-            </StyledCardsWrapper>
-            <Spacer size="lg" />
-          </StyledBank>
-        </Box>
-      </>
-    )
-  : <BankNotFound/>
-            }
-          </>
-        ) : (
-          <UnlockWallet />
-        )}
-      </>
+                  <Grid item xs={12} md={2} lg={2} className={classes.gridItem}>
+                    <Card className={classes.gridItem}>
+                      <CardContent style={{ textAlign: 'center' }}>
+                        <Typography>TVL</Typography>
+                        <Typography>
+                          $
+                          {statsOnPool?.TVL
+                            ? Number(Number(statsOnPool?.TVL).toFixed(0)).toLocaleString('en-US')
+                            : '-.--'}
+                        </Typography>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                </Grid>
+              </Box>
+
+              <Box mt={5}>
+                <StyledBank>
+                  <StyledCardsWrapper>
+                    <StyledCardWrapper>
+                      <Harvest bank={bank} />
+                    </StyledCardWrapper>
+                    <Spacer />
+                    <StyledCardWrapper>{<Stake bank={bank} />}</StyledCardWrapper>
+                  </StyledCardsWrapper>
+                  <Spacer size="lg" />
+                </StyledBank>
+              </Box>
+            </>
+          ) : (
+            <BankNotFound />
+          )}
+        </>
+      ) : (
+        <UnlockWallet />
+      )}
+    </>
   );
 };
 
-const LPTokenHelpText = ({bank}) => {
+const LPTokenHelpText = ({ bank }) => {
   // const grapeFinance = useGrapeFinance();
 
   // let pairName: string;
@@ -218,7 +214,7 @@ const LPTokenHelpText = ({bank}) => {
     <Card>
       <CardContent>
         <StyledLink href={'#'} target="_blank">
-          <span style={{color: '#000'}}>
+          <span style={{ color: '#000' }}>
             Provide liquidity for {'pairname'} on {'exchange'}
           </span>
         </StyledLink>
@@ -289,11 +285,11 @@ const StyledOutline = styled.div`
   z-index: -1;
 `;
 
-const StyledOutlineWrapper = styled.div`    
-    position: relative;
-    background: #08090d;
-    border-radius: 0px;
-    box-shadow: 0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05)
+const StyledOutlineWrapper = styled.div`
+  position: relative;
+  background: #08090d;
+  border-radius: 0px;
+  box-shadow: 0px 2px 12px -8px rgba(25, 19, 38, 0.1), 0px 1px 1px rgba(25, 19, 38, 0.05);
 `;
 
 const StyledMenuItem = withStyles({
