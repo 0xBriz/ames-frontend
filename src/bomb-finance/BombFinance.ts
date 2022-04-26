8124cc1caef9189302be568467fff78ceebebc53
quartz-defi <quartz.defi@protonmail.com>
2022-04-26 19:32:11 +0100

    Update alloc

--- src/bomb-finance/BombFinance.ts
+++ src/bomb-finance/BombFinance.ts
@@ -398,15 +398,15 @@ export class BombFinance {
     const rewardPerSecond = await poolContract.tSharePerSecond();
 
     if (depositTokenName === 'AMES-UST-LP') {
-      return rewardPerSecond.mul(28500).div(59500);
+      return rewardPerSecond.mul(30000).div(59500);
     } else if (depositTokenName === 'AMES-ASHARE-LP') {
       return rewardPerSecond.mul(0).div(59500);
     } else if (depositTokenName === '1QSHARE') {
       return rewardPerSecond.mul(0).div(59500);
     } else if (depositTokenName === 'ASHARE-UST-LP') {
-      return rewardPerSecond.mul(19000).div(59500);
+      return rewardPerSecond.mul(19250).div(59500);
     } else if (depositTokenName === 'AMES') {
-      return rewardPerSecond.mul(12000).div(59500);
+      return rewardPerSecond.mul(10250).div(59500);
     } else {
       return rewardPerSecond.mul(0).div(59500);
     }
