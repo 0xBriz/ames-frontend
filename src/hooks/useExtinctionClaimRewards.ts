3c73d28a9683962954be80b1a99509708f26154c
0xBriz <chaindevone@gmail.com>
2022-06-27 04:06:43 -0400

    refresh after claim

--- src/hooks/useExtinctionClaimRewards.ts
+++ src/hooks/useExtinctionClaimRewards.ts
@@ -2,13 +2,21 @@ import { useCallback } from 'react';
 import useBombFinance from './useBombFinance';
 import useHandleTransactionReceipt from './useHandleTransactionReceipt';
 import { ContractName } from '../bomb-finance';
+import useExtinctionPools from './useExtinctionPools';
 
 const useExtinctionClaimRewards = (poolName: ContractName) => {
   const bombFinance = useBombFinance();
   const handleTransactionReceipt = useHandleTransactionReceipt();
+  const { resfreshPools } = useExtinctionPools();
 
   const handleReward = useCallback(() => {
-    handleTransactionReceipt(bombFinance.claimExtinctionPool(poolName), ` Claim rewards from AMES pool `);
+    handleTransactionReceipt(
+      bombFinance.claimExtinctionPool(poolName).then((tx) => {
+        resfreshPools();
+        return tx;
+      }),
+      ` Claim rewards from AMES pool `,
+    );
   }, [poolName, bombFinance, handleTransactionReceipt]);
 
   return { doClaim: handleReward };
